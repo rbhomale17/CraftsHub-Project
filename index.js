2ab56032ca1ba7e268532f9cf4273f388a41cdaa
Ajit Kumar Roy <ajit607029@gmail.com>
2023-03-31 21:02:55 +0530

    done

--- index.js
+++ index.js
@@ -6,3 +6,31 @@ setInterval(function(){
     counter=1;
   }
 }, 4000)
+
+// <-- Navbar JS -->
+
+var navbar = document.querySelector('.navbar');
+var sticky = navbar.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add('sticky')
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
+window.onscroll = function() {stickyNav()};
+
+// <-- Navbar JS -->
+
+let f_name = JSON.parse(localStorage.getItem("name"));
+let sign_in = document.getElementById("sign-in");
+
+sign_in.innerText = "";
+sign_in.innerText = f_name[f_name.length-1];
+
+
+
+
+
+
+
